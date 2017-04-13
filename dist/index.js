3f4ae3e6ac9dc5cb1c64cfc6756c307e82a087e9
Latikaa <latikaa@policybazaar.com>
2017-04-13 13:40:57 +0530

    changed index file for api call

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,27 @@
 'use strict';
 
 require('./generator-runtime');
+
 module.exports = require('./json-rules-engine');
+module.exports = require('../src/chatbot-rules/common_rules');
+
+var express = require('express');
+var app = express();
+var sync    = require('synchronize');
+var await = sync.await;
+
+app.get('/rules', function (req, res) {
+    var anc = module.exports.callRules();
+    console.log("anc is" + anc);
+    res.end(anc);
+});
+
+var server = app.listen(8081, function () {
+
+  //var host = server.address().address;
+  //var port = server.address().port;
+    var host = "10.0.56.81";
+    var port = 8081;
+    console.log("Example app listening at http://%s:%s", host, port);
+
+});
